041406b64cb99dab1bf4e19d64ab662f4271793c
sxf <15389443705@189.cn>
2019-10-17 10:33:50 +0800

    first commit

---
+++ src/main/java/com/cc/hl/App.java
@@ -0,0 +1,24 @@
+package com.cc.hl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+
+public class App
+
+{
+
+    public static void main( String[] args )
+
+    {
+
+        SpringApplication.run(App.class, args);
+
+    }
+
+}
---
+++ src/main/java/com/cc/hl/demo/HelloController.java
@@ -0,0 +1,19 @@
+package com.cc.hl.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@RequestMapping("hello2")
+public class HelloController {
+
+       @RequestMapping("")
+
+       public String hello() {
+
+              return "helloworld2";
+
+       }
+
+}
